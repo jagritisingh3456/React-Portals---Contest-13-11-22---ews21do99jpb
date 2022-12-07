b436107dd7fd3881b7e4f33cfcdc97bca692d063
Jagriti Singh <108628421+jagritisingh3456@users.noreply.github.com>
2022-12-08 01:33:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,24 @@ import PortalTextArea from './PortalTextArea.js';
 
 //complete the function 
 const App = () => {
+  const [inputValue,setInputValue] = useState("");
+  const [textAreaValue,setTextAreaValue] = useState("");
+  
+  const handleInput = (e)=>{
+    setInputValue(e.target.value);
+  }
 
-
+  const buttonclick =()=>{
+    setTextAreaValue(inputValue);
+    setInputValue("");
+  }
 
   return (
     
     <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+      <input id="input" value={inputValue} onChange={handleInput}></input><br/>
+      <PortalButton buttonclick={buttonclick} /><br/>
+      <PortalTextArea value={textAreaValue}/>
     </div>
   )
 }
